0a8ed5bd23f08e162953b7cbfca1599ead5227cb
Iuliia91 <you@example.com>
2021-12-04 19:52:15 +0200

    last changes

--- Js/index.js
+++ Js/index.js
@@ -6,32 +6,3 @@ createCards(generatmokCart())
 sortByRating()
 sortByDate()
 sortbyTotalSum()
-
-// for (let i = 0; i < block.length; i++) {
-//   block[i].addEventListener('click', (Event) => {
-//     let { target } = Event
-//     let s = generatmokCart()
-//     if (target == document.getElementById('rating')) {
-//       document.getElementById('rating').classList.add('button_checked')
-
-//       s.sort((a, b) => {
-//         return a.rating - b.rating
-//       })
-//       createCards(s)
-//     } else {
-//       document.getElementById('rating').classList.remove('button_checked')
-//     }
-
-//     if (target == document.getElementById('releaseDate')) {
-//       document.getElementById('releaseDate').classList.toggle('button_checked')
-//     } else {
-//       document.getElementById('releaseDate').classList.remove('button_checked')
-//     }
-
-//     if (target == document.getElementById('boxOffice')) {
-//       document.getElementById('boxOffice').classList.toggle('button_checked')
-//     } else {
-//       document.getElementById('boxOffice').classList.remove('button_checked')
-//     }
-//   })
-// }
